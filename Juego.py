66776a199b9207cc2c0080f9f2b539e4da6f05b1
Franco <franco.rlam@estudiante.uc.cl>
2026-03-26 16:54:39 -0300

    Parte 2

--- Juego.py
+++ Juego.py
@@ -7,3 +7,4 @@ def pasos ():
     lista_opciones = ["piedra", "papel", "tijeras", "lagartija", "spock"]
     mano_jugador = (input("Elige una de las opciones a continuación: Piedra, Papel, Tijeras, Lagartija o Spock: ")).lower()
     mano_npc = lista_opciones[r.randint(0,4)]
+    
